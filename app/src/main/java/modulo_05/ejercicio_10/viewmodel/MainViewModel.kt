31ec54f015314f13757c1bbf9225953be6ef34a8
reborned <bushineko@gmail.com>
2024-09-18 22:33:39 -0300

    Final

--- app/src/main/java/modulo_05/ejercicio_10/viewmodel/MainViewModel.kt
+++ app/src/main/java/modulo_05/ejercicio_10/viewmodel/MainViewModel.kt
@@ -3,6 +3,6 @@ package modulo_05.ejercicio_10.viewmodel
 class MainViewModel {
 
     fun addPatient(nombre: String) {
-        // Agrega el paciente a tu lista (e.g., una lista mutable en el ViewModel)
+
     }
 }
